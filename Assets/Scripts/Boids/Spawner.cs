7c360e6bbc3bc0898dc623ffe78aa73922361574
zkfakaii <zkfakaii@gmail.com>
2024-10-30 15:21:51 -0300

    fifth 30/10

--- Assets/Scripts/Boids/Spawner.cs
+++ Assets/Scripts/Boids/Spawner.cs
@@ -12,7 +12,7 @@ public class Spawner : MonoBehaviour {
     public Color colour;
     public GizmoType showSpawnRegion;
 
-    void Update () {
+    void Start () {
         for (int i = 0; i < spawnCount; i++) {
             Vector3 pos = transform.position + Random.insideUnitSphere * spawnRadius;
             Boid boid = Instantiate (prefab);
